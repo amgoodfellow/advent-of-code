b9c891a4746c6ecb6e5fdb3efa6ce54b107887ab
Aaron Goodfellow <amgoodfellow@protonmail.com>
2023-12-02 18:18:48 -0500

    add some 2023 stuff

---
+++ 2023/flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs = {
+    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-23.05"; };
+    flake-utils = { url = "github:numtide/flake-utils"; };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in with pkgs; {
+        devShell = pkgs.mkShell {
+          buildInputs = [ clojure cargo clippy rust-analyzer rustc rustfmt ];
+        };
+      });
+}
